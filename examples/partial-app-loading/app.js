e7fb20f4b6a92a8bc2d27d06be6166c077dcb990
thomasheathcote <thomasheathcote@hotmail.com>
2017-02-09 13:23:00 +0100

    change partial-app

--- examples/partial-app-loading/app.js
+++ examples/partial-app-loading/app.js
@@ -8,21 +8,6 @@ var Link = Router.Link;
 var AsyncReactComponent = {
   loadedComponent: null,
 
-  load: function() {
-    if (this.constructor.loadedComponent) {
-      return;
-    }
-
-    this.bundle(function(component) {
-      this.constructor.loadedComponent = component;
-      this.forceUpdate();
-    }.bind(this));
-  },
-
-  componentDidMount: function() {
-    setTimeout(this.load, 1000); // feel it good
-  },
-
   render: function() {
     var component = this.constructor.loadedComponent;
     return component ? component(this.props) : this.preRender();
